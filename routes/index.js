c6e1ae40fba4f3745892dd0c527235bcb05c45d8
Jose Moreno <morenojr90@yahoo.com>
2018-07-19 08:34:11 -0700

    Add GET and POST register routes res.sends

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 // GET /register
 router.get('/register', function(req, res, next) {
-
+  return res.send("Register today");
 })
+
+// POST /register
+router.post('/register', function(req, res, next) {
+  return res.send("User created.")
+})
+
 
 // GET /
 router.get('/', function(req, res, next) {
